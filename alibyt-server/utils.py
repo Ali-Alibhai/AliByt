6a68ad7c6adc98ada7cdb85967f9b93a94e262ca
Ali Alibhai <aalibh4@uwo.ca>
2025-02-18 16:31:33 -0500

    paths

--- alibyt-server/utils.py
+++ alibyt-server/utils.py
@@ -2,6 +2,6 @@ import json
 
 def load_apps_config():
     """ Loads the apps_config.json file and returns available apps. """
-    CONFIG_PATH = "/home/aalibh4/AliByt/alibyt-server/apps_config.json"
+    CONFIG_PATH = "/home/ali/AliByt/alibyt-server/apps_config.json"
     with open(CONFIG_PATH, "r") as file:
         return json.load(file)
